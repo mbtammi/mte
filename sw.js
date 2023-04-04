82202dd9439fcf625d7b2c761381837d19220017
Miro <miro@Miros-MacBook-Pro.local>
2023-04-04 14:12:45 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bdd9c6a31507dde98307.js"
+    "url": "webpack-runtime-f0b5e658d29f4b0b2db8.js"
   },
   {
     "url": "framework-d91dc22f9c6070178d07.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.e729b8f74b21a4f6ecac.css"
   },
   {
-    "url": "app-5dcf14ebdeb2e241be01.js"
+    "url": "app-421313f940ebbeba20c4.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6bd82ece67d7f5811022d219c0baf0c4"
+    "revision": "8a1738f28764cfea1d61713ff273557a"
   },
   {
     "url": "polyfill-afa68794cf9a69af341a.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-5dcf14ebdeb2e241be01.js`))) {
+  if (!resources || !(await caches.match(`/app-421313f940ebbeba20c4.js`))) {
     return await fetch(event.request)
   }
 
