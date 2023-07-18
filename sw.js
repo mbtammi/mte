e26d2d89837a781e6cc399f96a7670fcb77d3d01
Miro Tammi <mirotammi@Miros-MacBook-Pro-2.local>
2023-07-18 10:13:41 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f9b554a5a3643929b90a.js"
+    "url": "webpack-runtime-b5e378930656f43d4a3f.js"
   },
   {
     "url": "framework-d91dc22f9c6070178d07.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.e729b8f74b21a4f6ecac.css"
   },
   {
-    "url": "app-e23442a47e06b61cf917.js"
+    "url": "app-d5c67439ff208fe54fa7.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6e760b7aac9256ae43548ac888138e9a"
+    "revision": "120d8f3d1f6f1a88c8497ae1b3fd0353"
   },
   {
     "url": "polyfill-afa68794cf9a69af341a.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-e23442a47e06b61cf917.js`))) {
+  if (!resources || !(await caches.match(`/app-d5c67439ff208fe54fa7.js`))) {
     return await fetch(event.request)
   }
 
